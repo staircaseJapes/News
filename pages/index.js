59e9846859a764141f0f3f9d9c7fb058d1acdc85
staircaseJapes <staircaseJapes@users.noreply.github.com>
2025-07-20 23:13:15 +0100

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>The Naughty Times (Static Version)</h1>
-      <p>Alice Tusa accused of global pillow theft, cookie raids, and excessive napping at G7 summits.</p>
-      <p>This site was exported statically for Netlify. Full features available in the dynamic version.</p>
-    </div>
-  );
-}
