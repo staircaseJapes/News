c1f6295e5f9b959c958b709aa3bf933ccc7c350d
staircaseJapes <staircaseJapes@users.noreply.github.com>
2025-07-20 23:14:34 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>The Naughty Times</h1>
+      <p><strong>Editor’s Note:</strong> Alice Tusa continues to confound world expectations with her escalating mischief.</p>
+    </div>
+  );
+}
