f4547c5ef08a348b853785648bf503b3961a61ae
staircaseJapes <staircaseJapes@users.noreply.github.com>
2025-07-22 21:52:52 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
 };
+module.exports = nextConfig;
 
-export default nextConfig;
 
 
 // OpenGraph support added
