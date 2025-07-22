91e4a3b1a270ee6394e320e08ac2d0b5c0e50c11
staircaseJapes <staircaseJapes@users.noreply.github.com>
2025-07-22 22:04:08 +0300

    as

--- 404.js
+++
@@ -1,9 +0,0 @@
-
-export default function Custom404() {
-  return (
-    <div className="notFound">
-      <h1>404</h1>
-      <p>Alice has hidden this page behind a cushion fort.</p>
-    </div>
-  );
-}
